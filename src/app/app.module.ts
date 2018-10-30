37fb215fa6ac0310f409d19db6e2339b380ee4a8
Muhammad Junaid Hassan <muhammadjunaidhassan1@gmail.com>
2018-10-30 13:31:37 +0500

    removed spark

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -70,12 +70,7 @@ import { AutocompleteProvider } from '../providers/autocomplete/autocomplete';
 import { Geolocation } from '@ionic-native/geolocation';
 
 const firebase = {
-  apiKey: "AIzaSyAbZplqw7rUDD2Nha-hkNI8-EAHh0J771c",
-  authDomain: "handyman-ea0e1.firebaseapp.com",
-  databaseURL: "https://handyman-ea0e1.firebaseio.com",
-  projectId: "handyman-ea0e1",
-  storageBucket: "handyman-ea0e1.appspot.com",
-  messagingSenderId: "221396946660"
+
 }
 
 @NgModule({
